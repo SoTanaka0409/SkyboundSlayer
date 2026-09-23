e235429513d18347dd1c962c80b609a928013404
SoTanaka0409 <tky2502039@stu.o-hara.ac.jp>
2026-09-23 11:37:20 +0900

    使用していないフォルダの削除

--- Tools/Scripts/PowerShell/patch_boss_jump3.ps1
+++
@@ -1,8 +0,0 @@
-$content = Get-Content -Encoding Default -Path Source\EnemyBoss_1.cpp -Raw
-
-$search1 = "if \(mJumpSpeed < 10\.0f\) mJumpSpeed = 10\.0f;"
-$replace1 = "// if (mJumpSpeed < 10.0f) mJumpSpeed = 10.0f; // removed to prevent overshooting"
-
-$content = $content -replace $search1, $replace1
-
-Set-Content -Encoding Default -Path Source\EnemyBoss_1.cpp -Value $content
